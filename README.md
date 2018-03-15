59c60151436a2bd1a09d6991170e4789ba12ab41
daoge1205 <870818962@qq.com>
2018-03-15 09:49:47 +0800

    php email

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dailystudy
+day 20180315 email.php
+
---
+++ email.php
@@ -0,0 +1,6 @@
+<?php 
+02.$to = "xxxxxxxx@qq.com"; 
+03.$subject = "Test mail"; 
+04.$message = "Hello! This is a simple email message."; 
+05.mail($to,$subject,$message); 
+06.?>  
